6a445aaae62a70bdb29ce07b15925da281c9f077
Michal Dubovsky <michal.dubovsky@cern.ch>
2024-04-09 00:31:02 +0200

    Improvements of power supply regulator

--- src/PowerSuplyMonitor/PowerSuplyMonitor.ino
+++ src/PowerSuplyMonitor/PowerSuplyMonitor.ino
@@ -4,6 +4,9 @@
 #include <Adafruit_GFX.h>
 #include <string>
 
+#include <stdlib.h>
+#include <cmath>
+
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 64
 
@@ -18,6 +21,42 @@ Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
 
 double total_charge = 0;
 int32_t last_time = 0;
+
+
+double RandomUniform()  {
+    return (double(rand())+1) / (double(RAND_MAX)+2);
+}
+
+double RandomUniform(float a, float b)  {
+    if (a > b) {
+        return RandomUniform(b, a);
+    }
+    // Be carefull about the brackts, otherwise RAND_MAX will overflow
+    return (b-a)*RandomUniform() + a;
+}
+
+
+std::string get_message(float value, std::string unit) {
+    if (value >= 0.8) {
+        return std::to_string(value).substr(0,5) + " " + unit;
+    }
+    else  {
+        return std::to_string(value*1000).substr(0,5) + " m" + unit;
+    }
+}
+
+std::string get_time_string(int time_milis) {
+    int time_seconds = time_milis/1000;
+    int time_minutes = time_seconds/60;
+    int time_hours = time_minutes/60;
+
+    time_seconds %= 60;
+    time_minutes %= 60;
+
+    return  ((time_hours >= 10) ? "" : "0") + std::to_string(time_hours) + ":" +
+            ((time_minutes >= 10) ? "" : "0") + std::to_string(time_minutes) + ":" +
+            ((time_seconds >= 10) ? "" : "0") + std::to_string(time_seconds);
+}
 
 void setup() {
     display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
@@ -37,15 +76,6 @@ void setup() {
 
 }
 
-std::string get_message(float value, std::string unit) {
-    if (value >= 0.8) {
-        return std::to_string(value).substr(0,5) + " " + unit;
-    }
-    else  {
-        return std::to_string(value*1000).substr(0,5) + " m" + unit;
-    }
-}
-
 void loop() {
     display.clearDisplay();
 
@@ -53,23 +83,49 @@ void loop() {
     display.setTextColor(WHITE);
     display.setCursor(0, 0);
 
+    const int next_time = millis() + 1000;
 
-    float voltage = VOLTAGE_CONST*analogRead(VOLTAGE_PIN);
-    float current = CURRENT_CONST*analogRead(CURRENT_PIN);
+    float avg_voltage = 0;
+    float avg_current = 0;
+    int n_samples = 0;
 
-    total_charge += current*(millis() - last_time)/3600000.0;
-    last_time = millis();
+    while (millis() < next_time) {
+        const float voltage = VOLTAGE_CONST*analogRead(VOLTAGE_PIN);
+        const float current = CURRENT_CONST*analogRead(CURRENT_PIN);
 
-    const std::string voltage_message = get_message(voltage, "V");
-    const std::string current_message = get_message(current, "A");
+        n_samples++;
+
+        avg_voltage += voltage;
+        avg_current += current;
+
+        delay(RandomUniform(5,20));
+    }
+
+    if (n_samples == 0) {
+        return;
+    }
+
+    const int time_now = millis();
+    const int time_step = time_now - last_time;
+    last_time = time_now;
+
+    avg_voltage /= n_samples;
+    avg_current /= n_samples;
+
+
+    total_charge += avg_current*time_step/3600000.0;
+
+    const std::string voltage_message = get_message(avg_voltage, "V");
+    const std::string current_message = get_message(avg_current, "A");
     const std::string charge_message = get_message(total_charge, "Ah");
 
     display.println(voltage_message.c_str());
     display.println(current_message.c_str());
     display.println(charge_message.c_str());
 
+    const std::string time_message = get_time_string(last_time);
+    display.println(time_message.c_str());
+
     display.display();
 
-    int delay_step = 500;
-    delay(delay_step);
 }
