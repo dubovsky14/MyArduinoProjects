8de2cca55a8d1dc004fcdfec17fa6b9884a05db7
Michal Dubovsky <michal.dubovsky@cern.ch>
2024-04-08 17:47:28 +0200

    Adding button for voltage decrease

--- src/DewHeaterRegulator/DewHeaterRegulator.ino
+++ src/DewHeaterRegulator/DewHeaterRegulator.ino
@@ -1,17 +1,19 @@
 #include <Arduino.h>
 
+#include <Wire.h>
 
 using namespace std;
 
-const int button_output_switch_index      = 0;
-const int button_voltage_regulator_index  = 1;
-const int range_leds_indices[]    = {2,3,4,5};
+const int button_output_switch_index     = 8;
+const int button_voltage_increase_index  = 7;
+const int button_voltage_decrease_index  = 6;
+const int range_leds_indices[]    = {14,15,26,27,28,29};
 const int range_leds_indices_size = sizeof(range_leds_indices)/sizeof(range_leds_indices[0]);
-const int output_leds_indices[]   = {6,7};
+const int output_leds_indices[]   = {3,4,5};
 const int output_leds_indices_size = sizeof(output_leds_indices)/sizeof(output_leds_indices[0]);
-const int output_voltage_pins[]   = {28,29};
+const int output_voltage_pins[]   = {0,1,2};
 const int output_voltage_pins_size = sizeof(output_voltage_pins)/sizeof(output_voltage_pins[0]);
-int output_voltages[]             = {0,0};
+int output_voltages[]             = {0,0,0};
 
 int current_output_pin = 0;
 int last_change_time = 0;
@@ -23,6 +25,18 @@ void switch_output_pin() {
     Serial.print("New output pin: ");
     Serial.println(current_output_pin);
 }
+
+void decrease_output_voltage() {
+    const unsigned int max_value = pow(2, range_leds_indices_size) - 1;
+    output_voltages[current_output_pin] = (output_voltages[current_output_pin] - 1) % (max_value + 1);
+
+
+    Serial.print("Current output: ");
+    Serial.print(current_output_pin);
+    Serial.print("Setting new voltage: ");
+    Serial.println(output_voltages[current_output_pin]);
+}
+
 
 void increase_output_voltage() {
     const unsigned int max_value = pow(2, range_leds_indices_size) - 1;
@@ -55,10 +69,14 @@ void update_leds() {
 }
 
 void set_pwm_pins() {
-    int time = millis();
+    const int time = millis();
+    const int min_pulse_width = 50;
+    const int max_value = pow(2, range_leds_indices_size) - 1;
+    const int max_pulse_width = max_value*min_pulse_width;
+
     for (unsigned int i = 0; i < output_voltage_pins_size; i++) {
-        const float value_fraction = float(output_voltages[i]) / (pow(2, range_leds_indices_size)-1.);
-        if (time % 1000 < value_fraction * 1000) {
+        const float value_fraction = float(output_voltages[i]) / max_value;
+        if (time % max_pulse_width < output_voltages[i] * min_pulse_width) {
             digitalWrite(output_voltage_pins[i], HIGH);
         } else {
             digitalWrite(output_voltage_pins[i], LOW);
@@ -71,7 +89,8 @@ void setup() {
 
     // put your setup code here, to run once:
     pinMode(button_output_switch_index, INPUT);
-    pinMode(button_voltage_regulator_index, INPUT);
+    pinMode(button_voltage_increase_index, INPUT);
+    pinMode(button_voltage_decrease_index, INPUT);
     for (const int i_led : range_leds_indices) {
         pinMode(i_led, OUTPUT);
     }
@@ -115,15 +134,27 @@ void loop() {
         update_leds();
     }
 
-    bool voltage_regulator_button_pressed = false;
-    while (digitalRead(button_voltage_regulator_index) == HIGH) {
-        voltage_regulator_button_pressed = true;
+    bool voltage_increase_button_pressed = false;
+    while (digitalRead(button_voltage_increase_index) == HIGH) {
+        voltage_increase_button_pressed = true;
         last_change_time = millis();
         delay(100);
-        Serial.println("button_voltage_regulator_index pressed");
+        Serial.println("button_voltage_increase_index pressed");
     }
-    if (voltage_regulator_button_pressed) {
+    if (voltage_increase_button_pressed) {
         increase_output_voltage();
+        update_leds();
+    }
+
+    bool voltage_decrease_button_pressed = false;
+    while (digitalRead(button_voltage_decrease_index) == HIGH) {
+        voltage_decrease_button_pressed = true;
+        last_change_time = millis();
+        delay(100);
+        Serial.println("button_voltage_decrease_index pressed");
+    }
+    if (voltage_decrease_button_pressed) {
+        decrease_output_voltage();
         update_leds();
     }
 }
