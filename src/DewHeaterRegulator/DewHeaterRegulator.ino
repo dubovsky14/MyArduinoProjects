17124580a0ffcf00f99fa779a97ee62a18af6f0e
Michal Dubovsky <michal.dubovsky@cern.ch>
2024-04-10 22:48:51 +0200

    Adjusting DewHeaterRegulator code to OLED display

--- src/DewHeaterRegulator/DewHeaterRegulator.ino
+++ src/DewHeaterRegulator/DewHeaterRegulator.ino
@@ -1,21 +1,39 @@
-#include <Arduino.h>
+
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <string>
+
+#include <stdlib.h>
+#include <cmath>
+
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
+
+#define OLED_ADDR   0x3C
+
+
+#define VOLTAGE_PIN 26
+#define VOLTAGE_CONST 0.0244
+#define CURRENT_PIN 27
+#define CURRENT_CONST 0.01135
 
 using namespace std;
 
-const int button_output_switch_index     = 8;
-const int button_voltage_increase_index  = 7;
-const int button_voltage_decrease_index  = 6;
-const int range_leds_indices[]    = {14,15,26,27,28,29};
-const int range_leds_indices_size = sizeof(range_leds_indices)/sizeof(range_leds_indices[0]);
-const int output_leds_indices[]   = {0,1,2};
-const int output_leds_indices_size = sizeof(output_leds_indices)/sizeof(output_leds_indices[0]);
-const int output_voltage_pins[]   = {3,4,5};
-const int output_voltage_pins_size = sizeof(output_voltage_pins)/sizeof(output_voltage_pins[0]);
-int output_voltages[]             = {0,0,0};
+const int32_t button_output_switch_index     = 26;
+const int32_t button_voltage_increase_index  = 15;
+const int32_t button_voltage_decrease_index  = 14;
+const int32_t output_leds_indices[]   = {6,7,8};
+const int32_t output_leds_indices_size = sizeof(output_leds_indices)/sizeof(output_leds_indices[0]);
+const int32_t output_voltage_pins[]   = {0,1,2};
+const int32_t output_voltage_pins_size = sizeof(output_voltage_pins)/sizeof(output_voltage_pins[0]);
+const int32_t max_voltage             = 100;
+int32_t output_voltages[]             = {0,0,0};
 
-int current_output_pin = 0;
-int last_change_time = 0;
+int32_t current_output_pin = 0;
+int32_t last_change_time = 0;
 
+Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
 
 void switch_output_pin() {
     current_output_pin = (current_output_pin + 1) % output_voltage_pins_size;
@@ -25,9 +43,7 @@ void switch_output_pin() {
 }
 
 void decrease_output_voltage() {
-    const unsigned int max_value = pow(2, range_leds_indices_size) - 1;
-    output_voltages[current_output_pin] = (output_voltages[current_output_pin] - 1) % (max_value + 1);
-
+    output_voltages[current_output_pin] = (output_voltages[current_output_pin] + max_voltage) % (max_voltage+1);
 
     Serial.print("Current output: ");
     Serial.print(current_output_pin);
@@ -37,9 +53,7 @@ void decrease_output_voltage() {
 
 
 void increase_output_voltage() {
-    const unsigned int max_value = pow(2, range_leds_indices_size) - 1;
-    output_voltages[current_output_pin] = (output_voltages[current_output_pin] + 1) % (max_value + 1);
-
+    output_voltages[current_output_pin] = (output_voltages[current_output_pin] + 1) % (max_voltage+1);
 
     Serial.print("Current output: ");
     Serial.print(current_output_pin);
@@ -48,32 +62,35 @@ void increase_output_voltage() {
 }
 
 void turn_off_all_leds() {
-    for (const int i_led : range_leds_indices) {
+    for (const int32_t i_led : output_leds_indices) {
         digitalWrite(i_led, LOW);
     }
-    for (const int i_led : output_leds_indices) {
-        digitalWrite(i_led, LOW);
-    }
+    display.clearDisplay();
+    display.display();
 }
 
 void update_leds() {
-    for (const int i_led : output_leds_indices) {
+    for (const int32_t i_led : output_leds_indices) {
         digitalWrite(i_led, (i_led == output_leds_indices[current_output_pin]) ? HIGH : LOW);
     }
+    display.clearDisplay();
 
-    for (unsigned int i = 0; i < range_leds_indices_size; i++) {
-        digitalWrite(range_leds_indices[i], output_voltages[current_output_pin] & (1 << i) ? HIGH : LOW);
-    }
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    const std::string message = std::to_string(output_voltages[current_output_pin]) + " %";
+    display.println(message.c_str());
+
+    display.display();
 }
 
 void set_pwm_pins() {
-    const int time = millis();
-    const int min_pulse_width = 50;
-    const int max_value = pow(2, range_leds_indices_size) - 1;
-    const int max_pulse_width = max_value*min_pulse_width;
+    const int32_t time = millis();
+    const int32_t min_pulse_width = 50;
+    const int32_t max_pulse_width = max_voltage*min_pulse_width;
 
-    for (unsigned int i = 0; i < output_voltage_pins_size; i++) {
-        const float value_fraction = float(output_voltages[i]) / max_value;
+    for (uint32_t i = 0; i < output_voltage_pins_size; i++) {
+        const float value_fraction = float(output_voltages[i]) / max_voltage;
         if (time % max_pulse_width < output_voltages[i] * min_pulse_width) {
             digitalWrite(output_voltage_pins[i], HIGH);
         } else {
@@ -89,24 +106,29 @@ void setup() {
     pinMode(button_output_switch_index, INPUT);
     pinMode(button_voltage_increase_index, INPUT);
     pinMode(button_voltage_decrease_index, INPUT);
-    for (const int i_led : range_leds_indices) {
-        pinMode(i_led, OUTPUT);
-    }
-    for (const int i_led : output_leds_indices) {
+
+    for (const int32_t i_led : output_leds_indices) {
         pinMode(i_led, OUTPUT);
     }
 
-    for (const int i_led : range_leds_indices) {
-        digitalWrite(i_led, HIGH);
-    }
-    for (const int i_led : output_leds_indices) {
+    for (const int32_t i_led : output_leds_indices) {
         digitalWrite(i_led, HIGH);
     }
 
-    for (const int i_pin : output_voltage_pins) {
+    for (const int32_t i_pin : output_voltage_pins) {
         pinMode(i_pin, OUTPUT);
         digitalWrite(i_pin, LOW);
     }
+
+    display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+    display.clearDisplay();
+
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("Hello");
+
+    display.display();
 
     delay(5000);
     Serial.print("Setup done");
